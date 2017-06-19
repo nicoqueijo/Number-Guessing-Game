8ff5cf511f18463f769e3ab64b928fc5518dbe37
Nicolas <nicoqueijo@hotmail.com>
2017-06-19 13:17:06 -0400

    documented project

--- NumberGuessingGame/Form1.cs
+++ NumberGuessingGame/Form1.cs
@@ -8,6 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Author: Nicolas Queijo
+
+/// <summary>
+/// In this number-guessing game, the user will enter a number between 1 and 100
+/// to try to guess a secret value. The program will answer with one of three possible
+/// responses: Either the number is too low, it is too high, or it is the secret number.
+/// </summary>
 namespace NumberGuessingGame
 {
     public partial class Form1 : Form
@@ -16,7 +23,7 @@ namespace NumberGuessingGame
         private const int OFFSET = 1;
         private const string HIGHER = "Higher!";
         private const string LOWER = "Lower!";
-        private const string INVALID_INPUT = "Invalid input. Try entering a number.";
+        private const string INVALID_INPUT = "Invalid input. Try entering an integer.";
         private const string CONGRATS_MESSAGE = "Congratulations! You won!\nWould you like to play again?";
         private const string NUM_OUT_OF_RANGE = "Number out of range. Try a number between 1-100.";
         private const string RESTART = "Restart Game";
@@ -32,11 +39,32 @@ namespace NumberGuessingGame
             InitializeComponent();
         }
 
+        /// <summary>
+        /// The main controls for the game become visible once the user clicks on the play button.
+        /// </summary>
+        /// <param name="sender">Unused but required</param>
+        /// <param name="e">Unused but required</param>
         private void clickToPlayLabel_Click(object sender, EventArgs e)
         {
             gameGroupBox.Visible = true;
         }
 
+        /// <summary>
+        /// This button is responsible for validating user input and determining if the game is
+        /// won. It begins by checking if the user entered an integer. If that is not the case
+        /// the user is notified and asked to enter an integer. When the user finally enters an
+        /// integer it is checked to see if it falls within the permitted range of 1-100. If this
+        /// is not the case the user is notified and asked to re-enter an integer in the permitted
+        /// range. Once the user entered an integer in the permitted range it is tested to see if
+        /// the number is less, greater, or equal to the secret number. If it is less/greater the
+        /// program tells the user that the secret number is lower/higher and the attempts variable
+        /// is incremented. If the user's number is equal to the secret number a message is shown
+        /// informing the user he/she won and is prompted to exit or play again. If the user decides
+        /// to play again the number-of-games variable is incremented and a new secret number is 
+        /// generated for the new game.
+        /// </summary>
+        /// <param name="sender">Unused but required</param>
+        /// <param name="e">Unused but required</param>
         private void guessButton_Click(object sender, EventArgs e)
         {
             if (int.TryParse(guessTextBox.Text, out userGuess))
@@ -70,15 +98,10 @@ namespace NumberGuessingGame
                 {
                     this.Close();
                 }
-
             }
             else if (guessTextBox.Text != "")
             {
                 MessageBox.Show(INVALID_INPUT);
-            }
-            else
-            {
-                // do nothing
             }
         }
     }
